a610fa7d48b6b8e7071fd99c1ee50f9584525dda
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-03-15 13:28:26 -0700

    Posting Messages Working

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   gql,
   useQuery,
+  useMutation,
 } from "@apollo/client";
-import { Container } from "shards-react";
+import { Container, Row, Col, FormInput, Button } from "shards-react";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
@@ -20,6 +21,12 @@ const GET_MESSAGES = gql`
       content
       user
     }
+  }
+`;
+
+const POST_MESSAGE = gql`
+  mutation($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
   }
 `;
 
@@ -57,8 +64,9 @@ const Messages = ({ user }) => {
           )}
           <div
             style={{
-              background: user === messageUser ? "blue" : "#e5e6ea",
+              background: user === messageUser ? "green" : "#e5e6ea",
               color: user === messageUser ? "white" : "black",
+              margin: "10px",
               padding: "1em",
               borderRadius: "1em",
               maxWidth: "60%",
@@ -73,10 +81,61 @@ const Messages = ({ user }) => {
 };
 
 const Chat = () => {
+  const [state, stateSet] = useState({ user: "Nathan", content: "" });
+  const [postMessage] = useMutation(POST_MESSAGE);
+
+  const onSend = () => {
+    if (state.content.length > 0) {
+      postMessage({
+        variables: state,
+      });
+    }
+    stateSet({
+      ...state,
+      content: "",
+    });
+  };
+
   return (
     <div>
       <Container>
-        <Messages user="Jonny" />
+        <Messages user={state.user} />
+        <Row>
+          <Col xs={2} style={{ padding: 0 }}>
+            <FormInput
+              label="User"
+              value={state.user}
+              onChange={(evt) =>
+                stateSet({
+                  ...state,
+                  user: evt.target.value,
+                })
+              }
+            />
+          </Col>
+          <Col xs={8}>
+            <FormInput
+              label="Content"
+              value={state.content}
+              onChange={(evt) =>
+                stateSet({
+                  ...state,
+                  content: evt.target.value,
+                })
+              }
+              onKeyUp={(evt) => {
+                if (evt.keyCode === 13) {
+                  onSend();
+                }
+              }}
+            />
+          </Col>
+          <Col xs={2} style={{ padding: 0 }}>
+            <Button onClick={() => onSend()} style={{ width: "100%" }}>
+              Send
+            </Button>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
